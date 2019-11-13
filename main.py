42f8fe94506e536661e911ab168d21a5404cae47
victorlebou <victor.leboutillier@gmail.com>
2019-11-13 11:49:08 -0500

    Première fonction

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('hello')
+import argparse
+def analyser_commande():
+    parser = argparse.ArgumentParser(description="Jeu Quoridor - phase 1")
+    parser.add_argument('-l', '--lister' metavar='',help="Lister les identifiants de vos 20 dernières parties")
+    parser.add_argument('identification', nargs='+', metavar= 'idul', help='IDUL du joueur')    
+    args = parser.parse_args()
+    return args
